d18923423dbf9fc3fb0cfa45f21381e168c014f1
Eric Estorani <88246810+pie0my@users.noreply.github.com>
2021-08-02 17:07:40 -0300

    Delete filescurso directory

--- filescurso/arquivo.py
+++
@@ -1,8 +0,0 @@
-def algo(num=0):
-    try:
-        if num:
-            return int(num) + 5
-        else:
-            return 'utilize um numero'
-    except ValueError as err:
-        return err
